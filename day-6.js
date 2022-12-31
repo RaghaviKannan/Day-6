8e2067422f031f6c0950914cac912504c278e125
RaghaviKannan <86419053+RaghaviKannan@users.noreply.github.com>
2022-12-31 18:49:43 +0530

    Add files via upload

---
+++ day-6.js
@@ -0,0 +1,11 @@
+class Movie {
+  constructor(title, studio, rating="PG") {
+    this.title = title;
+    this.studio = studio;
+    this.rating = rating;
+  }
+  static getPG(movies) {
+    return movies.filter(movie => movie.rating === "PG");
+  }
+}
+const movie = new Movie("Casino Royale", "Eon Productions", "PG-13");
